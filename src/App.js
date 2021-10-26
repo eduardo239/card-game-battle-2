ef5f9d217401f1b8920fa2e3d6c56c1db6eecf17
eduardo239 <eucrieiumaconta@gmail.com>
2021-10-25 21:28:19 -0300

    testando imagem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import './css/App.css';
 
-import HeroCard from './components/card/Hero';
-import MonsterCard from './components/card/Monster';
-import MapCard from './components/card/Map';
-import ItemShopCard from './components/card/ItemShop';
-import ItemCard from './components/card/Item';
-import MenuGame from './components/menu/Game';
-import PageFight from './components/game/Fight';
-import Confirm from './components/modal/Confirm';
-import PageGiftTrap from './components/game/GiftTrap';
-import Positions from './components/game/Positions';
-
 import Navigation from './components/ui/Navigation';
-import {} from './store/user';
+
 import {
   fetchHeroes,
   fetchItems,
   fetchMaps,
-  fetchMonsters,
-  getRandomNumber
+  fetchMonsters
 } from './store/game';
 import { useSelector, useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router';
@@ -32,18 +20,12 @@ import Page4 from './components/page/Page4';
 import PageGame from './components/page/PageGame';
 import PageShop from './components/page/PageShop';
 
-import Alert from './components/messages/Alert';
-import MenuPlay from './components/menu/Play';
-import Dice from './components/game/Dice';
 import Home from './components/page/Home';
 
 function App() {
   const dispatch = useDispatch();
 
   const { isError, isLoading } = useSelector(state => state.game.status);
-  const { dice } = useSelector(state => state.game);
-
-  const [n, setN] = useState('');
 
   useEffect(() => {
     dispatch(fetchHeroes());
@@ -90,40 +72,11 @@ function App() {
 
       <hr />
 
-      <br />
-      {/* alert */}
-      <Alert type='success' message=' A simple primary alert—check it out!' />
+      {/* <Alert type='success' message=' A simple primary alert—check it out!' />
       <Alert type='danger' message=' A simple primary alert—check it out!' />
-      <Alert type='info' message=' A simple primary alert—check it out!' />
+      <Alert type='info' message=' A simple primary alert—check it out!' /> */}
 
-      <br />
-      {/* position */}
-      <Positions />
-      <br />
-      {/* menu */}
-      <section className='flex'>
-        <input
-          value={n}
-          onChange={e => setN(e.target.value)}
-          placeholder='Meows..'
-        />
-
-        <button
-          className='btn btn-secondary'
-          onClick={() => dispatch(getRandomNumber(1, 6))}
-        >
-          random {dice}
-        </button>
-        <button className='btn btn-secondary'>if odd</button>
-      </section>
-      <br />
-      {/* item */}
-      <section>
-        <Dice dice={dice} />
-      </section>
-      <br />
-      {/* form */}
-      <section>
+      {/* <section>
         <div className='field'>
           <label htmlFor=''>Name</label>
           <input type='text' placeholder='Meows..' />
@@ -137,74 +90,47 @@ function App() {
           <input type='text' />
         </div>
         <button className='btn btn-primary'>start</button>
-      </section>
+      </section> */}
 
-      <br />
-      {/* buttons */}
-      <section>
+      {/* <section>
         <button className='btn btn-primary'>start</button>
         <button className='btn btn-secondary'>start</button>
         <button className='btn btn-secondary'>next</button>
-      </section>
-      <br />
-      {/* button size */}
+      </section> */}
+
+      {/* <MenuGame />
+      <MenuPlay /> */}
       <section>
-        <button className='btn btn-small btn-primary'>primary</button>
-        <button className='btn btn-primary'>use item</button>
-        <button className='btn btn-big btn-secondary'>primary</button>
+        <div className='card'>
+          <div className='card-poster'>
+            <img
+              src={process.env.PUBLIC_URL + '/assets/items/po-sm-hp.jpg'}
+              alt='123123123'
+            />
+          </div>
+          <div className='card-body'>
+            <div>
+              <h5>HELA</h5>
+            </div>
+            <div>
+              <span className='card-item'>
+                <span>100</span>
+              </span>
+              <span className='card-item'>
+                <span>100</span>
+              </span>
+            </div>
+            <div>
+              <span className='card-item'>
+                <span>100</span>
+              </span>
+              <span className='card-item'>
+                <span>WIND</span>
+              </span>
+            </div>
+          </div>
+        </div>
       </section>
-      <br />
-      {/* button with icon */}
-      <MenuGame />
-      <MenuPlay />
-      <br />
-      {/* hero */}
-      <HeroCard>
-        <div>
-          <button className='btn btn-primary'>Selecionar</button>
-        </div>
-      </HeroCard>
-      <br />
-      {/* monster */}
-      <MonsterCard>
-        <div>
-          <button className='btn btn-primary'>Selecionar</button>
-        </div>
-      </MonsterCard>
-      <br />
-      {/* map */}
-      <MapCard>
-        <div>
-          <button className='btn btn-primary'>Selecionar</button>
-        </div>
-      </MapCard>
-      <br />
-      {/* card shop */}
-      <ItemShopCard>
-        <div>
-          <button className='btn btn-primary'>Comprar</button>
-        </div>
-      </ItemShopCard>
-      <br />
-      {/* item */}
-      <ItemCard>
-        <div>
-          <button className='btn btn-primary'>Selecionar</button>
-        </div>
-      </ItemCard>
-      <br />
-
-      {/* item */}
-      <Confirm />
-      <br />
-
-      {/* item */}
-      <br />
-      <PageGiftTrap />
-
-      {/*  */}
-      <br />
-      <PageFight />
     </div>
   );
 }
